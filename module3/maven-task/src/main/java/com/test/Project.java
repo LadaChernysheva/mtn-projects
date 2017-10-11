b806c535b7cd3c54c58ea560c05b7e48c93e0130
LadaChernysheva <31819131+LadaChernysheva@users.noreply.github.com>
2017-10-11 18:11:55 +0300

    Update Project.java
    changes for test

--- module3/maven-task/src/main/java/com/test/Project.java
+++ module3/maven-task/src/main/java/com/test/Project.java
@@ -7,10 +7,22 @@ class Project {
 
   public static void test() {
     System.out.println("test");
-  }
+      }
   
   public static void main(String[] args) {
     System.out.println("Hellow MTN");
+    
+    int x=7;
+    switch(x) {
+    case 1: 
+	    System.out.println("1");
+		break;
+	case 5: 
+	    System.out.println("5");
+		break;
+	case 7: 
+	    System.out.println("7");
+		break;
   }
 
 }
